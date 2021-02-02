0db8191593fb5f8b2d24691e6ccfa19eece50656
Utku Tarhan <utku.tarhan@hotmail.com>
2021-02-02 01:17:43 -0800

    Correction

--- frontend/view_model.py
+++ frontend/view_model.py
@@ -277,76 +277,6 @@ class MenuPage():
                 lines.append(EMPTY_LINE_ITEM)
         return MenuRendering(lines=lines, header=self.header, page_start=self.index, total_count=total_size)
 
-class SettingsPage():
-    def __init__(self, header, previous_page, has_sub_page=False, is_title = False):
-        self.index = 0
-        self.page_start = 0
-        self.header = header
-        self.has_sub_page = has_sub_page
-        self.previous_page = previous_page
-        self.is_title = is_title
-
-    def total_size(self):
-        return 0
-
-    def page_at(self, index):
-        return None
-
-    def nav_prev(self):
-        spotify_manager.run_async(lambda: spotify_manager.play_previous())
-
-    def nav_next(self):
-        spotify_manager.run_async(lambda: spotify_manager.play_next())
-
-    def nav_play(self):
-        spotify_manager.run_async(lambda: spotify_manager.toggle_play())
-
-    def get_index_jump_up(self):
-        return 1
-
-    def get_index_jump_down(self):
-        return 1
-
-    def nav_up(self):
-        jump = self.get_index_jump_up()
-        if(self.index >= self.total_size() - jump):
-            return
-        if (self.index >= self.page_start + MENU_PAGE_SIZE - jump):
-            self.page_start = self.page_start + jump
-        self.index = self.index + jump
-
-    def nav_down(self):
-        jump = self.get_index_jump_down()
-        if(self.index <= (jump - 1)):
-            return
-        if (self.index <= self.page_start + (jump - 1)):
-            self.page_start = self.page_start - jump
-            if (self.page_start == 1):
-                self.page_start = 0
-        self.index = self.index - jump
-
-    def nav_select(self):
-        return self.page_at(self.index)
-
-    def nav_back(self):
-        return self.previous_page
-
-    def render(self):
-        lines = []
-        total_size = self.total_size()
-        for i in range(self.page_start, self.page_start + MENU_PAGE_SIZE):
-            if (i < total_size):
-                page = self.page_at(i)
-                if (page is None) :
-                    lines.append(EMPTY_LINE_ITEM)
-                else:
-                    line_type = LINE_TITLE if page.is_title else \
-                        LINE_HIGHLIGHT if i == self.index else LINE_NORMAL
-                    lines.append(LineItem(page.header, line_type, page.has_sub_page))
-            else:
-                lines.append(EMPTY_LINE_ITEM)
-        return MenuRendering(lines=lines, header=self.header, page_start=self.index, total_count=total_size)
-
 class PlaylistsPage(MenuPage):
     def __init__(self, previous_page):
         super().__init__(self.get_title(), previous_page, has_sub_page=True)
@@ -512,7 +442,6 @@ class RootPage(MenuPage):
             NewReleasesPage(self),
             PlaylistsPage(self),
             SearchPage(self),
-            SettingsPage(self),
             NowPlayingPage(self, "Now Playing", NowPlayingCommand())
         ]
         self.index = 0
